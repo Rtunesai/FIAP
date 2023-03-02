ddd81a39552f2d352e43223add163f5d4ec4b79b
rtunes <ricmytv@gmail.com>
2023-03-02 09:51:16 -0300

    aula01

--- aula01/conceitos.py
+++ aula01/conceitos.py
@@ -12,6 +12,7 @@ altura = 1.70
 maior_idade = True
 # Boolean e bool()
 
+# type method
 valor = 25
 print(valor, type(valor))
 
@@ -29,9 +30,28 @@ valor1 = int(valor1)
 valor2 = "20"
 valor2 = int(valor2)
 
+# Concatenation
 resposta = valor1 + valor2
 
 print(resposta)
 
+
+# Format e f string
 print('Resposta = {}'. format(resposta))
 
+print(f"valor de resposta: {resposta} ")
+
+
+# Decimal
+a = 56.7
+
+# https://docs.python.org/2/library/string.html#grammar-token-format-spec
+# Referencia formatacao
+
+print("RS {:2.2f}". format(a))
+
+print(f'Valor de a = {a}')
+print(f'Valor de a = {a:6}')
+print(f'Valor de a = {a:06}')
+
+aula = "python".

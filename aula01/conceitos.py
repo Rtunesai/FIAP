a1f7745cac0c04d054ded2848244dd8777d1889a
rtunes <ricmytv@gmail.com>
2023-03-02 09:24:25 -0300

    aula01

---
+++ aula01/conceitos.py
@@ -0,0 +1,37 @@
+print('PYcharm')
+
+nome = 'Ricardo'
+# str()
+
+idade = 24
+# Integer e int()
+
+altura = 1.70
+# Real e float()
+
+maior_idade = True
+# Boolean e bool()
+
+valor = 25
+print(valor, type(valor))
+
+valor = '25'
+print(valor, type(valor))
+
+valor = 45.90
+print(valor, type(valor))
+
+valor = False
+print(valor, type(valor))
+
+valor1 = "10"
+valor1 = int(valor1)
+valor2 = "20"
+valor2 = int(valor2)
+
+resposta = valor1 + valor2
+
+print(resposta)
+
+print('Resposta = {}'. format(resposta))
+
